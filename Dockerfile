5fdc0a8e89d7d046952f62d7b5868a4a0482d018
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-12-22 14:05:19 +0900

    jstatでログ取得するようにした

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ RUN sh gradlew build
 # ENV JAVA_OPTS=""
 # ENTRYPOINTなどの実行するものはENVが適切
 # ENTRYPOINT [ "sh", "-c", "java $JAVA_OPTS -jar build/libs/JvmStudent-1.0-SNAPSHOT.jar" ]
-ENTRYPOINT [ "sh", "-c", "echo $JAVA_OPTS && java $JAVA_OPTS -jar build/libs/JvmStudent-1.0-SNAPSHOT.jar" ]
+ENTRYPOINT [ "sh", "-c", "echo $JAVA_OPTS && java $JAVA_OPTS -jar build/libs/JvmStudent-1.0-SNAPSHOT.jar & bash /app/jstat.sh" ]
--- jstat.sh
+++ jstat.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+echo "  S0     S1     E      O      M     CCS    YGC     YGCT    FGC    FGCT    CGC    CGCT     GCT    LGCC                 GCC     " > /app/logs/jstat.log
+sleep 5s
 while :
 do
-    jps | grep jar | awk '{print $1}' | xargs -I{} jstat -gccause {} >> /app/logs/jstat.log
+    PROCESS_ID=$( jps | grep jar | awk '{print $1}' )
+    if [ -z "${PROCESS_ID}" ]; then
+      # END
+      exit 0
+    fi
+    # jps | grep jar | awk '{print $1}' | xargs -I{} jstat -gccause {} | tail -1 >> /app/logs/jstat.log
+    jstat -gccause ${PROCESS_ID} | tail -1 >> /app/logs/jstat.log
     sleep 5s
 done
 
